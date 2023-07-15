751c241afcbfad93af2eb6ab51a9eb05630d61fb
Broken-life <yamfogar@gmail.com>
2023-07-15 16:10:44 -0300

    Ahora si, corre el servidor, y andan las migraciones

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls")), #inicio de la pagina
+    path("", views.index, name="index"), #pagina principal
     path("login/", include("apps.login.urls")), #apartado de login
     path("post/", include("apps.post.urls")), #apartado de post
 ]
