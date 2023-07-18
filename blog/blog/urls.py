8ed3619c9bbe57d2b4d5c700e4b931a1c9a8d880
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-07-17 21:35:47 -0300

    cambios a la fecha 17-07-2023

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"), #pagina principal
+    path("", views.home, name="home"), #pagina principal
     path("login/", include("apps.login.urls")), #apartado de login
     path("post/", include("apps.post.urls")), #apartado de post
     path("crear-articulo/", include("apps.post.urls")),
-    path('home/', views.home, name="home"),
+    #path('home/', views.home, name="home"),
     path("about/", views.about, name="acerca"),
     path('contact/',views.contacto, name="contacto"),
     path('categorias/', views.categorias, name="categorias"),
