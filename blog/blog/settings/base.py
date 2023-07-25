469e152a8596e05c489df5f1aac7e020a60a12f9
Broken-life <yamfogar@gmail.com>
2023-07-25 19:03:08 -0300

    xd

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -144,6 +144,7 @@ print(BASE_DIR)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "ppythoneers@gmail.com"
 EMAIL_HOST_PASSWORD = "123pythoneers!"
